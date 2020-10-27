0608335a61eea81e4da2401f4368b8d97acffa7c
Digambaran <arjunsreekumar.1@gmmail.com>
2020-10-27 15:48:24 +0530

    FIX--negative values starting with -1min and -1hr fixed

--- cdt_app/src/Counter.js
+++ cdt_app/src/Counter.js
@@ -2,12 +2,19 @@ import React, {useEffect} from "react";
 const calculateTimeLeft = (startingtime, pausingtime, total) => {
   const now = new Date().getTime();
   let difference = Math.floor(total - (now - startingtime));
-  return {
-    h: Math.floor((difference / 3600000) % 24),
-    m: Math.floor((difference / 1000 / 60) % 60),
-    s: Math.floor((difference / 1000) % 60),
-    ms: Math.floor((difference / 1) % 1000),
-  };
+  return difference >= 0
+    ? {
+        h: Math.floor((difference / 3600000) % 24),
+        m: Math.floor((difference / 1000 / 60) % 60),
+        s: Math.floor((difference / 1000) % 60),
+        ms: Math.floor((difference / 1) % 1000),
+      }
+    : {
+        h: Math.ceil((difference / 3600000) % 24),
+        m: Math.ceil((difference / 1000 / 60) % 60),
+        s: Math.ceil((difference / 1000) % 60),
+        ms: Math.ceil((difference / 1) % 1000),
+      };
 };
 function Counter({
   startingtime,
