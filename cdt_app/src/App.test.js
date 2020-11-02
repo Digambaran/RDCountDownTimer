de88f13a9c668b2cb781cb151b9308b6d3717aa8
Digambaran <arjunsreekumar.1@gmmail.com>
2020-11-02 09:29:15 +0530

    tests added

--- cdt_app/src/App.test.js
+++ cdt_app/src/App.test.js
@@ -1,16 +1,46 @@
 import React from "react";
-import {render} from "@testing-library/react";
+import {render, fireEvent} from "@testing-library/react";
 import App from "./App";
+describe("Displays headers and buttons and inputs", () => {
+  test("Displays Headers", () => {
+    const {getByText} = render(<App />);
 
-test("Displays headers and buttons and inputs", () => {
-  const {getByText, getAllByPlaceholderText} = render(<App />);
-  getByText("Count Down Timer");
-  getByText("Lap Timer");
-  getByText("start");
-  getByText("pause");
-  getByText("resume");
-  getByText("reset");
-  getAllByPlaceholderText("hours");
-  getAllByPlaceholderText("minutes");
-  getAllByPlaceholderText("seconds");
+    getByText("Count Down Timer");
+    getByText("Lap Timer");
+  });
+
+  test("Displays Buttons", () => {
+    const {getByText} = render(<App />);
+
+    getByText("start");
+    getByText("pause");
+    getByText("resume");
+    getByText("reset");
+  });
+
+  test("Displays Input boxes", () => {
+    const {getAllByPlaceholderText} = render(<App />);
+
+    expect(getAllByPlaceholderText("hours").length).toBe(1);
+    expect(getAllByPlaceholderText("minutes").length).toBe(1);
+    expect(getAllByPlaceholderText("seconds").length).toBe(1);
+  });
+});
+
+describe("Input Boxes", () => {
+  test("should Update on Entering value", () => {
+    const {queryByPlaceholderText} = render(<App />);
+    const HrBox = queryByPlaceholderText("hours");
+    const MinBox = queryByPlaceholderText("minutes");
+    const SecBox = queryByPlaceholderText("seconds");
+
+    fireEvent.change(HrBox, {target: {value: "12"}});
+    expect(HrBox.value).toBe("12");
+
+    fireEvent.change(MinBox, {target: {value: "12"}});
+    expect(MinBox.value).toBe("12");
+
+    fireEvent.change(SecBox, {target: {value: "12"}});
+    expect(SecBox.value).toBe("12");
+  });
 });
